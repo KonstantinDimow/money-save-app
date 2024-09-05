cd6f65c8f78374155eea31632966253432702170
Konstantin Dimow <konstantin.dimow@googlemail.com>
2024-09-05 12:14:27 +0200

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'save-money-app';
+
+  income = 0;
+  outcome = 0;
+
+
+  ngOnInit(): void {
+
+  }
+
+  setIncome(event: KeyboardEvent){
+    this.income = +(event.target as HTMLInputElement).value;
+  }
+
+  setOutcome(event: KeyboardEvent){
+    this.outcome = +(event.target as HTMLInputElement).value;
+  }
 }
